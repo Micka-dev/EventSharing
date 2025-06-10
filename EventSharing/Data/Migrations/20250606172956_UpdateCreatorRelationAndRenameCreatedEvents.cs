eb999644326d5f1e8fb11dc56ec6a0354983d802
Mickaël Douceré <mickamickadou@gmail.com>
2025-06-10 09:27:58 +0200

    Migration modification du nom CeatedEvents en CreatedEvents

---
+++ EventSharing/Data/Migrations/20250606172956_UpdateCreatorRelationAndRenameCreatedEvents.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventSharing.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCreatorRelationAndRenameCreatedEvents : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
